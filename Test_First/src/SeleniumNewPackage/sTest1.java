c8ae2bfdcaacf9268c5e234680bd01f17ea2623e
LuisValenzo <jluis.diazv@gmail.com>
2017-01-02 10:04:21 -0600

    }test

--- Test_First/src/SeleniumNewPackage/sTest1.java
+++ Test_First/src/SeleniumNewPackage/sTest1.java
@@ -4,7 +4,8 @@ public class sTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test2");
+		String sTxt = "Test2";
+		System.out.println(sTxt);
 	}
 
 }
