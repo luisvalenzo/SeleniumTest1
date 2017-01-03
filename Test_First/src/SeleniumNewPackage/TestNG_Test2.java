5e2378b3f0ee4db723eb5fcbf94aac044335df9b
LuisValenzo <jluis.diazv@gmail.com>
2017-01-02 19:21:34 -0600

    .

--- Test_First/src/SeleniumNewPackage/TestNG_Test2.java
+++ Test_First/src/SeleniumNewPackage/TestNG_Test2.java
@@ -25,15 +25,16 @@ public class TestNG_Test2 {
 	public WebDriver wd;
 	
   @Test
-  public void f() {
+  public void SelectFlight() {
 	  wd.findElement(By.xpath("//*[@id='login-search-wrap']/div[3]/div/div[3]/form/div[7]/div[1]/cityselector/div[1]/div/a/span/img")).click();
-	//*[@id="airport_popup_title"]
+
 	  Assert.assertTrue(wd.findElements(By.xpath("//*[@id='airport_popup_title']")).size()!=0, "Airport PopUp");
 	  Verify.verify(wd.findElements(By.xpath("//*[@id='airport_popup_title']")).size()!=0);
 	  wd.findElement(By.xpath("//*[@id='airports-list-wrapper']/div/div/div/div[1]/a[1]")).click();
 	  //System.out.println(wd.findElement(By.xpath("//*[@id='airports-list-wrapper']/div/div/div/div[1]/a[1]")).getAttribute("innertext"));
 	  Utilities.SelectCity("C", wd);
   }
+  
   @BeforeMethod
   public void beforeMethod() {
 	  wd.get("https://www.jetblue.com/");
